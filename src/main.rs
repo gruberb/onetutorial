e9bfbef7d49eec8552210329c169c3298db34b45
Bastian Gruber <code@recv.online>
2021-09-17 15:23:18 +0200

    Fetch BTC price from CMC

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
+    let mut params = HashMap::new();
+    params.insert("symbol", "BTC");
+    
+    let client = reqwest::Client::new();
+    let resp = client
+        .get(" https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest")
+        .header("X-CMC_PRO_API_KEY", "YOUR_API_KEY")
+        .query(&params)
+        .send()
         .await?;
+
+    let resp = resp.text().await?;
+
     println!("{:#?}", resp);
+
     Ok(())
 }
