9fdbaa50f015f34ea33785861bb5bfe30fef927a
Bastian Gruber <code@recv.online>
2021-09-17 16:37:37 +0200

    Add impl block, impl Display, create Vec<Currency>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,46 @@
 use std::collections::HashMap;
+use std::fmt;
 
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct CMCResponse {
+    data: HashMap<String, Currency>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Currency {
+    name: String,
+    symbol: String,
+    quote: Quotes,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Quotes(HashMap<String, Quote>);
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Quote {
+    price: f64,
+    percent_change_7d: f64,
+}
+
+impl fmt::Display for Currency {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Name: {}, Symbol: {} Price: {} Change(7d): {}%", 
+            self.name, 
+            self.symbol, 
+            self.quote.0.get("USD").unwrap().price.to_string(), 
+            self.quote.0.get("USD").unwrap().percent_change_7d.to_string()
+        )
+    }
+}
+
+impl CMCResponse {
+    fn get_currency(&self, currency: &str) -> Option<&Currency> {
+        self.data.get(currency)
+    }
+}
+ 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
      dotenv::dotenv().ok();
@@ -16,10 +57,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .query(&params)
         .send()
         .await?;
-
-    let resp = resp.text().await?;
-
-    println!("{:#?}", resp);
+    
+    let prices = resp.json::<CMCResponse>().await?;
+   
+    if let Some(bitcoin) = prices.get_currency("BTC") {
+        println!("{}", bitcoin);
+    } else {
+        println!("Bitcoin is not in the list");
+    }
 
     Ok(())
 }
