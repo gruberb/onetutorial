10391edb3f170225ffaef499259a1fbaaf6d3278
Bastian Gruber <code@recv.online>
2021-09-17 15:30:06 +0200

    Add reading cmc_key from .env

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+     dotenv::dotenv().ok();
+    
+    let cmc_pro_api_key = dotenv::var("CMC_PRO_API_KEY").expect("CMC key not set");
+
     let mut params = HashMap::new();
     params.insert("symbol", "BTC");
     
     let client = reqwest::Client::new();
     let resp = client
         .get(" https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest")
-        .header("X-CMC_PRO_API_KEY", "YOUR_API_KEY")
+        .header("X-CMC_PRO_API_KEY", cmc_pro_api_key)
         .query(&params)
         .send()
         .await?;
